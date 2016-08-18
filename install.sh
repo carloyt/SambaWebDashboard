937643aacfa34410c87c088f624c5ce2abc309fa
carloyt <carloyt@free.fr>
2016-08-18 12:45:13 +0000

    modifié :         install.sh Ajouté à install.sh : copie des fichiers dans le répertoire DocumentRoot; optimisation de la création des fichiers de config Samba.

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-#apt-get --assume-yes install samba
+apt-get --assume-yes install samba
 
 DOCUMENTROOT_DEFAULT="/var/www/html/"
 read -p "Please enter DocumentRoot path with the \"/\" at the end [$DOCUMENTROOT_DEFAULT]: " DOCUMENTROOT
 DOCUMENTROOT="${DOCUMENTROOT:-$DOCUMENTROOT_DEFAULT}"
-#echo "Document root = $DOCUMENTROOT"
 
 SMBROOTPATH="$DOCUMENTROOT""smb/"
-#echo $SMBROOTPATH
 
 mkdir "$SMBROOTPATH"
 echo "Le dossier $SMBROOTPATH a bien été créé." 
-#touch /etc/samba/smb.conf
 
 SAMBACONFPATH_DEFAULT="/etc/samba/"
 read -p "Please enter Samba configuration path with the \"/\" at the end (if you don't know put default) [$SAMBACONFPATH_DEFAULT]: " SAMBACONFPATH
@@ -21,11 +18,13 @@ SAMBACONFPATH="${SAMBACONFPATH:-$SAMBACONFPATH_DEFAULT}"
 SAMBACONFPATH="$SAMBACONFPATH""conf/"
 mkdir "$SAMBACONFPATH"
 echo "Le dossier $SAMBACONFPATH a bien été créé."
+touch "$SAMBACONFPATH""shares.conf"
 
-cd $SMBROOTPATH
-wget https://github.com/carloyt/SambaWebDashboard/blob/master/LICENSE
-mkdir files/
-cd files/
-wget https://github.com/carloyt/SambaWebDashboard/blob/master/files/add.php
-wget https://github.com/carloyt/SambaWebDashboard/blob/master/files/conf.php
-wget https://github.com/carloyt/SambaWebDashboard/blob/master/files/edit.php
+cd SambaWebDashboard/files/
+cp add.php $SMBROOTPATH
+cp edit.php $SMBROOTPATH
+cp conf.php $SMBROOTPATH
+
+#cd $SMBROOTPATH
+#mkdir files/
+#cd files/
